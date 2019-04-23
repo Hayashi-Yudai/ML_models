5a881b69b45f4c470233f1f63551e9b245298df2
Yudai Hayashi <yudai0913haya@gmail.com>
2019-04-23 22:14:38 +0900

    add regularization and batch-normalization in conv2d

--- UNet/model.py
+++ UNet/model.py
@@ -1,5 +1,53 @@
 import tensorflow as tf
 import numpy as np
 
-def conv2d(inputs, filters, activation=tf.nn.relu):
-  return tf.layers.conv2d(inputs=inputs, filters=filters, kernel_size=3, padding='SAME', activation=activation)
+def conv2d(
+  inputs, filters, activation=tf.nn.relu, l2_reg=None, is_training=False,
+  momentum=0.95, epsilon=0.001
+  ):
+  """
+  convolutional layer. If the l2_reg is a float number, L2 regularization is imposed.
+  Args:
+    inputs: tf.Tensor
+    filters: Non-zero positive integer. The number of the filter 
+    activation: The activation function. The default is tf.nn.relu
+    l2_reg: None or a float. The strengthen of the L2 regularization
+    is_training: boolean (tf.Tensor). The default is False. If True, the batch normalization layer is added.
+    momentum: float. The hyper parameter of the batch normalization layer
+    epsilon: float. The hyper parameter of the batch normalization layer
+  Returns:
+    tf.Tensor
+  """
+  regularizer = tf.contrib.layers.l2_regularizer(scale=l2_reg) if l2_reg is not None else None
+  layer = tf.layers.conv2d(
+    inputs=inputs,
+    filters=filters,
+    kernel_size=3,
+    padding='SAME',
+    activation=activation,
+    kernel_regularizer=regularizer
+  )
+
+  if is_training:
+    layer = tf.layers.batch_normalization(
+      inputs=layer,
+      axis=-1,
+      momentum=momentum,
+      epsilon=epsilon,
+      center=True,
+      scale=True,
+      training=is_training
+    )
+
+  return layer
+
+
+def trans_conv(inputs, filters, activation=tf.nn.relu, kernel_size=2, strides=2):
+  layer = tf.layers.conv2d_transpose(
+    inputs=inputs,
+    filters=filters,
+    kernel_size=kernel_size,
+    strides=strides
+  )
+
+  return layer
--- UNet/tests/test_model.py
+++ UNet/tests/test_model.py
@@ -8,17 +8,44 @@ import numpy as np
 import tensorflow as tf
 import unittest
 
-class TestLayers(unittest.TestCase):
-  def test_convolutional_layer(self):
+class TestEachLayers(unittest.TestCase):
+  def test_convolution_layer(self):
     inputs = np.ones((3, 128, 128, 3))
+
+    tf.reset_default_graph()
     x = tf.placeholder(tf.float32, [None, 128, 128, 3])
     layer = model.conv2d(x, filters=64)
+    layer_with_l2 = model.conv2d(x, filters=64, l2_reg=0.01)
+    layer_with_bn = model.conv2d(x, filters=64, is_training=True)
     init = tf.global_variables_initializer()
 
     with tf.Session() as sess:
       init.run()
       outputs = sess.run(layer, feed_dict={x: inputs})
-      self.assertEqual(outputs.shape, (3, 128, 128, 64))
+      outputs_with_l2 = sess.run(layer_with_l2, feed_dict={x: inputs})
+      outputs_with_bn = sess.run(layer_with_bn, feed_dict={x: inputs})
+
+    self.assertEqual(outputs.shape, (3, 128, 128, 64))
+    self.assertEqual(outputs_with_l2.shape, (3, 128, 128, 64))
+    self.assertEqual(outputs_with_bn.shape, (3, 128, 128, 64))
+
+    self.assertFalse(np.all(outputs == outputs_with_l2))
+    self.assertFalse(np.all(outputs == outputs_with_bn))
+    self.assertFalse(np.all(outputs_with_l2 == outputs_with_bn))
+
+  def test_transposed_convolution_layer(self):
+    inputs = np.ones((3, 128, 128, 3))
+
+    tf.reset_default_graph()
+    x = tf.placeholder(tf.float32, [None, 128, 128, 3])
+    layer = model.trans_conv(x, filters=64)
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+      init.run()
+      outputs = sess.run(layer, feed_dict={x: inputs})
+
+    self.assertEqual(outputs.shape, (3, 256, 256, 64))
 
 if __name__ == '__main__':
   unittest.main()
