09fc6b73d80516a559f633920fa889a8f7179d24
Yudai Hayashi <yudai0913haya@gmail.com>
2019-06-08 15:38:56 +0900

    Add codes to load and generate dataset

--- ACoL/prepare_data.py
+++ ACoL/prepare_data.py
@@ -1,6 +1,9 @@
 import os
+import numpy as np
+from PIL import Image
 
 IMAGE_DIR = '/home/yudai/Pictures/raw-img'
+CLASS_NUM = 10
 
 def image_files(train=True) -> list:
   suffix = '/train/' if train else '/validation/'
@@ -11,8 +14,44 @@ def image_files(train=True) -> list:
     tmp = []
     for f in os.listdir(IMAGE_DIR + suffix + category):
       if f.endswith('.png') or f.endswith('.jpg') or f.endswith('.jpeg'):
-        tmp.append(f)
+        tmp.append(suffix + f'/{category}/' + f)
     
     images.append(tmp)
 
   return images
+
+
+def make_dataset(train=True) -> np.ndarray:
+  images = image_files(train)
+  class_idx = 0
+  dataset = []
+  for category in range(len(images)):
+    label = np.array([0]*CLASS_NUM)
+    label[class_idx] = 1
+    for img in images[category]:
+      pil_img = Image.open(IMAGE_DIR + img)
+      pil_img = pil_img.resize([224, 224])
+      np_img = np.asanyarray(pil_img, dtype=np.float32)
+      np_img /= 255.0
+
+      dataset.append((np_img, label))
+
+    class_idx += 1
+
+  return np.array(dataset)
+
+
+def generate_dataset(batch_size: int, train=True):
+  dataset = make_dataset(train)
+  np.random.shuffle(dataset)
+  print(len(dataset))
+  bt_dataset = []
+  for data in dataset:
+    if len(bt_dataset) == batch_size:
+      yield bt_dataset
+      bt_dataset = []
+    else:
+      bt_dataset.append(data)
+
+if __name__ == '__main__':
+  print(type(make_dataset(False)[0]))
--- ACoL/tests/test_prepare_data.py
+++ ACoL/tests/test_prepare_data.py
@@ -4,13 +4,32 @@ import sys
 sys.path.append(FILE + '/../')
 
 import unittest
+import numpy as np
 import prepare_data
 
 
 class TestLoadData(unittest.TestCase):
   def test_datafile(self):
-    image_dirs = prepare_data.image_files()
+    image_dirs = prepare_data.image_files(train=False)
     self.assertEqual(len(image_dirs), 10)
+
+
+  def test_load_images(self):
+    img = prepare_data.make_dataset(train=False)
+
+    self.assertEqual(type(img), np.ndarray)
+    self.assertEqual(type(img[0]), np.ndarray)
+    self.assertEqual(img[0][0].shape, (224, 224, 3))
+    self.assertEqual(img[0][1].shape, (10,))
+    self.assertEqual(sum(img[0][1]), 1)
+
+
+  def test_generate_dataset(self):
+    gen = prepare_data.generate_dataset(batch_size=10, train=False)
+
+    for dataset in gen:
+      self.assertEqual(len(dataset), 10)
+      break
 
 
 if __name__ == '__main__':
