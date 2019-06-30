3bda5e65c20568e14b55d1eefa482b902aac9a9d
Yudai Hayashi <yudai0913haya@gmail.com>
2019-06-30 12:00:08 +0900

    Changed some default hyperparameters

--- ArcFace/metrics.py
+++ ArcFace/metrics.py
@@ -3,7 +3,7 @@ from tensorflow.keras.layers import Layer
 import tensorflow.keras.backend as K
 
 class ArcFace(Layer):
-    def __init__(self, n_classes=1000, s=30.0, m=0.50, regularizer=None, **kwargs):
+    def __init__(self, n_classes=1000, s=64.0, m=0.5, regularizer=None, **kwargs):
         super(ArcFace, self).__init__(**kwargs)
         self.n_classes = n_classes
         self.s = s
