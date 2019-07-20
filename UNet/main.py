943050e2ed7ae11fc71a19a14dca67447dc56960
Yudai Hayashi <yudai0913haya@gmail.com>
2019-07-20 23:47:56 +0900

    Wrote the training process

--- UNet/main.py
+++ UNet/main.py
@@ -1,26 +1,77 @@
 import argparse
 import model
+import prepare_data
+import tensorflow as tf
+
+
+class DiceLossByClass:
+    def __init__(self, input_shape, class_num):
+        self.__input_h = input_shape[0]
+        self.__input_w = input_shape[1]
+        self.__class_num = class_num
+
+    def dice_coef(self, y_true, y_pred):
+        y_true = tf.keras.backend.flatten(y_true)
+        y_pred = tf.keras.backend.flatten(y_pred)
+        intersection = tf.keras.backend.sum(y_true * y_pred)
+        denominator = tf.keras.backend.sum(y_true) + tf.keras.backend.sum(y_pred)
+        if denominator == 0:
+            return 1
+        if intersection == 0:
+            return 1 / (denominator + 1)
+        return (2.0 * intersection) / denominator
+
+    def dice_coef_loss(self, y_true, y_pred):
+        # (N, h, w, ch)
+        y_true_res = tf.reshape(
+            y_true, (-1, self.__input_h, self.__input_w, self.__class_num)
+        )
+        y_pred_res = tf.reshape(
+            y_pred, (-1, self.__input_h, self.__input_w, self.__class_num)
+        )
+        y_trues = tf.unstack(y_true_res, axis=3)
+        y_preds = tf.unstack(y_pred_res, axis=3)
+
+        losses = []
+        for y_t, y_p in zip(y_trues, y_preds):
+            losses.append((1 - self.dice_coef(y_t, y_p)) * 3)
+
+        return tf.reduce_sum(tf.stack(losses))
 
 
 def get_parser():
     """
-  Set hyper parameters for training UNet.
-  """
+    Set hyper parameters for training UNet.
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--epoch", type=int, default=100)
-    parser.add_argument("-lr", "--learning_rate", type=float, default=0.0001)
+    parser.add_argument("--n_classes", type=int, default=2)
+    parser.add_argument("--epoch", type=int, default=100)
+    parser.add_argument("--learning_rate", type=float, default=0.0001)
     parser.add_argument(
-        "-tr", "--train_rate", type=float, default=0.8, help="ratio of training data"
+        "--train_rate", type=float, default=0.8, help="ratio of training data"
     )
-    parser.add_argument("-b", "--batch_size", type=int, default=20)
-    parser.add_argument(
-        "-l2", "--l2", type=float, default=0.05, help="L2 regularization"
-    )
+    parser.add_argument("--batch_size", type=int, default=20)
+    parser.add_argument("--l2", type=float, default=0.05, help="L2 regularization")
 
-    return parser
+    return parser.parse_args()
+
+
+def train(args):
+    lr = args.learning_rate
+    unet = model.UNet(args)
+    unet.compile(
+        optimizer=tf.keras.optimizers.Adam(lr),
+        loss=DiceLossByClass((388, 388), 2).dice_coef_loss,
+        metrics=["accuracy"],
+    )
+    unet.summary()
+
+    generator = prepare_data.data_gen(
+        "./dataset/raw_images/", "./dataset/segmented_images/", 2
+    )
+    unet.fit_generator(generator, steps_per_epoch=100, epochs=10)
 
 
 if __name__ == "__main__":
-    parser = get_parser().parse_args()
-    unet = model.UNet(classes=2)
-    unet.train(parser)
+    args = get_parser()
+    train(args)
--- UNet/prepare_data.py
+++ UNet/prepare_data.py
@@ -1,135 +1,3 @@
-import os
-import random
-import math
-
-import numpy as np
-from PIL import Image, ImageOps, ImageEnhance
-
-
-def preprocess(img, seg, img_list, seg_list, n_class, onehot):
-    """
-  Preprocess the image for training or validation
-
-  Parameters
-  ==========
-  img: image opened by PIL library. Original images
-  seg: image opened by PIL library. Segmented images
-  img_list, seg_list: list of np.ndarray that contains preprocessed images.
-  n_class: int that represents the number of the class
-  onehot: bool that determine wheter ones use one-hot representation 
-
-  Returns
-  =======
-  None
-  """
-    for i in range(10):
-        img2 = img
-        seg2 = seg
-        mark = [0, 1]
-        if seg2 is not None:
-            if np.random.choice(mark) == 1:
-                img2 = ImageOps.flip(img2)
-                seg2 = ImageOps.flip(seg2)
-            if np.random.choice(mark) == 1:
-                img2 = ImageOps.mirror(img2)
-                seg2 = ImageOps.mirror(seg2)
-
-        color_img = ImageEnhance.Color(img2)
-        change = np.random.rand() / 2.0 + 0.75
-        img2 = color_img.enhance(change)
-
-        img2 = img2.resize([128, 128])
-        img2 = np.asarray(img2, dtype=np.float32)
-        if seg2 is not None:
-            seg2 = seg2.resize([128, 128])
-            seg2 = np.asarray(seg2, dtype=np.int16)
-
-        if onehot and seg is not None:
-            identity = np.identity(n_class, dtype=np.int16)
-            seg2 = identity[seg2]
-        img_list.append(img2 / 255.0)
-        if seg2 is not None:
-            seg_list.append(seg2)
-
-    return None
-
-
-def load_data(image_dir, seg_dir, n_class, train_val_rate, onehot=True):
-    """
-  load images and segmented images.
-
-  Parameters
-  ==========
-  image_dir: string 
-    the directory of the raw images.
-  seg_dir: string
-    the directory of the segmented images.
-  n_class: int
-    the number of classes
-  train_val_rate: float
-    the ratio of training data to the validation data
-  onehot: bool
-
-  Returns
-  =======
-  the tuple.(
-    (training image, training segmented image),
-    (validation image, validation segmented image)
-    )
-  training/validation (segmented) images are the list of np.ndarray whose shape
-  is (128, 128, 3) (row image) and (128, 128, n_class) (segmented image)
-  """
-    row_img = []
-    segmented_img = []
-    images = os.listdir(image_dir)
-    random.shuffle(images)
-    for img in images:
-        if img.endswith(".png") or img.endswith(".jpg"):
-            split_name = os.path.splitext(img)
-            img = Image.open(os.path.join(image_dir, img))
-            if seg_dir is not None:
-                seg = Image.open(
-                    os.path.join(seg_dir, split_name[0] + "-seg" + split_name[1])
-                )
-            else:
-                seg = None
-
-            preprocess(img, seg, row_img, segmented_img, n_class, onehot=onehot)
-
-    train_val_border = int(len(row_img) * train_val_rate)
-    train_data = row_img[:train_val_border], segmented_img[:train_val_border]
-    validation_data = row_img[train_val_border:], segmented_img[train_val_border:]
-
-    return train_data, validation_data
-
-
-def generate_data(input_images, teacher_images, batch_size):
-    """
-  generate the pair of the raw image and segmented image.
-
-  Parameters
-  ========== 
-  inputs_images: list of the np.array
-  teacher_image: list of the np.array or None
-  batch_size: int
-
-  Returns
-  =======
-  """
-    batch_num = math.ceil(len(input_images) / batch_size)
-    input_images = np.array_split(input_images, batch_num)
-    if np.any(teacher_images is None):
-        teacher_images = np.zeros(batch_num)
-    else:
-        teacher_images = np.array_split(teacher_images, batch_num)
-
-    for i in range(batch_num):
-        yield input_images[i], teacher_images[i]
-
-
-####################################################################
-## New
-####################################################################
 import imgaug as iaa
 import numpy as np
 import os
@@ -138,33 +6,32 @@ from PIL import Image
 
 def data_gen(train_data, seg_data, batch_size):
     inputs = np.array(os.listdir(train_data))
-    shuffle = np.random.permutation(len(inputs))
     batch = len(inputs) // batch_size
     identity = np.identity(2, dtype=np.int16)
 
-    for b in np.array_split(inputs[shuffle], batch):
-        imgs = []
-        segs = []
-        for img_file in b:
-            img = Image.open(train_data + img_file).convert("RGB")
-            img = img.resize((572, 572))
-            img = np.asarray(img) / 255.0
-            imgs.append(img)
+    while True:
+        shuffle = np.random.permutation(len(inputs))
+        for b in np.array_split(inputs[shuffle], batch):
+            imgs = []
+            segs = []
+            for img_file in b:
+                img = Image.open(train_data + img_file).convert("RGB")
+                img = img.resize((572, 572))
+                img = np.asarray(img) / 255.0
+                imgs.append(img)
 
-            seg = Image.open(seg_data + img_file.split(".")[0] + "-seg.png")
-            seg = seg.resize((388, 388))
-            seg = np.asarray(seg)
-            seg = identity[seg]
-            segs.append(seg)
+                seg = Image.open(seg_data + img_file.split(".")[0] + "-seg.png")
+                seg = seg.resize((388, 388))
+                seg = np.asarray(seg)
+                seg = identity[seg]
+                segs.append(seg)
 
-        yield imgs, segs
+            yield np.array(imgs), np.array(segs)
 
 
 if __name__ == "__main__":
     data = "./dataset/raw_images/"
     seg = "./dataset/segmented_images/"
 
-    gen = data_gen(data, seg, 1)
+    gen = data_gen(data, seg, 4)
     x, y = next(gen)
-    print(x[0].shape)
-    print(y[0].shape)
