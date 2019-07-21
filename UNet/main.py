d04ff02e37a6fe9c95597d151c5ca3297481993b
Yudai Hayashi <yudai0913haya@gmail.com>
2019-07-21 10:08:16 +0900

    Fix loss function

--- UNet/main.py
+++ UNet/main.py
@@ -4,41 +4,6 @@ import prepare_data
 import tensorflow as tf
 
 
-class DiceLossByClass:
-    def __init__(self, input_shape, class_num):
-        self.__input_h = input_shape[0]
-        self.__input_w = input_shape[1]
-        self.__class_num = class_num
-
-    def dice_coef(self, y_true, y_pred):
-        y_true = tf.keras.backend.flatten(y_true)
-        y_pred = tf.keras.backend.flatten(y_pred)
-        intersection = tf.keras.backend.sum(y_true * y_pred)
-        denominator = tf.keras.backend.sum(y_true) + tf.keras.backend.sum(y_pred)
-        if denominator == 0:
-            return 1
-        if intersection == 0:
-            return 1 / (denominator + 1)
-        return (2.0 * intersection) / denominator
-
-    def dice_coef_loss(self, y_true, y_pred):
-        # (N, h, w, ch)
-        y_true_res = tf.reshape(
-            y_true, (-1, self.__input_h, self.__input_w, self.__class_num)
-        )
-        y_pred_res = tf.reshape(
-            y_pred, (-1, self.__input_h, self.__input_w, self.__class_num)
-        )
-        y_trues = tf.unstack(y_true_res, axis=3)
-        y_preds = tf.unstack(y_pred_res, axis=3)
-
-        losses = []
-        for y_t, y_p in zip(y_trues, y_preds):
-            losses.append((1 - self.dice_coef(y_t, y_p)) * 3)
-
-        return tf.reduce_sum(tf.stack(losses))
-
-
 def get_parser():
     """
     Set hyper parameters for training UNet.
@@ -56,13 +21,18 @@ def get_parser():
     return parser.parse_args()
 
 
+def loss(y_true, y_pred):
+    y_true = tf.keras.backend.flatten(y_true)
+    y_pred = tf.keras.backend.flatten(y_pred)
+
+    return tf.reduce_sum(-tf.log(tf.clip_by_value(y_pred, 1e-10, 1.0)) * y_true)
+
+
 def train(args):
     lr = args.learning_rate
     unet = model.UNet(args)
     unet.compile(
-        optimizer=tf.keras.optimizers.Adam(lr),
-        loss=DiceLossByClass((388, 388), 2).dice_coef_loss,
-        metrics=["accuracy"],
+        optimizer=tf.keras.optimizers.Adam(lr), loss=loss, metrics=["accuracy"]
     )
     unet.summary()
 
