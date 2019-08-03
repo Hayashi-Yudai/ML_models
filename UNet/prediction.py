14dcd66777a7190d600924a910a4b5fb65da56b0
Yudai Hayashi <yudai0913haya@gmail.com>
2019-08-03 23:39:47 +0900

    Add prediction code to check the ability

---
+++ UNet/prediction.py
@@ -0,0 +1,49 @@
+import model
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import tensorflow as tf
+from typing import Tuple
+
+
+class args:
+    n_classes = 2
+    l2 = 0.0
+    weights = "./params/model.h5"
+
+
+def prediction(img: np.ndarray) -> np.ndarray:
+    unet = model.UNet(args)
+    pred = unet.predict([img])
+
+    return pred
+
+
+def to_colormap(img: np.ndarray, original_shape: Tuple[int]) -> np.ndarray:
+    pred = np.argmax(img[0], axis=2)
+    ident = np.identity(3, dtype=np.uint8)
+    pred = ident[pred] * 255
+    pred = Image.fromarray(pred)
+    pred = pred.resize(original_shape)
+
+    return pred
+
+
+if __name__ == "__main__":
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    tf.keras.backend.set_session(tf.Session(config=config))
+
+    img_original = Image.open("./dataset/raw_images/cat-10.png").convert("RGB")
+    original_shape = img_original.size
+
+    img = img_original.resize((224, 224))
+    img = np.asarray(img)
+    img = (img - 127.5) / 128
+
+    pred = prediction(img.reshape(1, 224, 224, 3))
+    pred = to_colormap(pred, original_shape)
+
+    plt.imshow(img_original)
+    plt.imshow(pred, alpha=0.3)
+    plt.show()
